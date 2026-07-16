bc8200313242464d7bc44a1c5f6ed633cc2791a3
gman618371 <grayson123007@gmail.com>
2026-07-16 16:17:13 -0500

    fix: remove raspberrypi-ui-mods from deploy deps (conflicts with pi-greeter)

--- scripts/deploy-pi.sh
+++ scripts/deploy-pi.sh
@@ -46,7 +46,6 @@ install_system_packages() {
     curl git build-essential \
     chromium-browser unclutter xdotool \
     xserver-xorg x11-xserver-utils xinit \
-    raspberrypi-ui-mods \
     avahi-daemon libnss-mdns \
     sqlite3 jq
 
